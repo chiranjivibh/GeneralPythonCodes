d72ede6c5ac6847f5f8b6b857aac8f0f87934042
Chiranjivi Bhattarai <chiranjivibh@gmail.com>
2022-04-14 17:01:59 -0700

    Example of reading U3 labjack data and save
    This program is reading volatage using U3 labjack and save it each step data for future reference.

---
+++ Autosave_labjack_data.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 11:17:13 2016
+
+@author: cbhattar
+"""
+
+import u3
+#from time import gmtime, strftime
+import datetime
+import time, sys
+d = u3.U3()
+print d.configU3()
+stored_exception=None
+fname=datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%H%M%S_')+'particleloss_UV.txt'
+#fname=datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_')+'labjack.txt'
+x=1
+file=open(fname, 'a')
+file.write('Date,NO,NOx,UV,CO\n')
+file.close()
+while True:
+    file=open(fname, 'a') 
+    try:    
+        #file.write('input \n')
+        tstep=datetime.datetime.strftime(datetime.datetime.now(),"%Y%m%d %H:%M:%S")
+        CO=d.getAIN(0)
+        NOx=d.getAIN(1)
+        UV=d.getAIN(3)
+        NO=d.getAIN(2)
+        print tstep, UV
+        #file.write("%s,%2.5f,%2.5f,%2.5f,%2.5f\n" %(tstep,NO,NOx,UV,CO))
+        file.write("%s,%2.5f\n" %(tstep,UV))
+        time.sleep(1)
+        #print("Press CTRL +C to break operation")
+        file.close()
+        if stored_exception:
+              break
+        x+=1
+    except KeyboardInterrupt:
+        print "[CTRL+C detected]" 
+        stored_exception=sys.exc_info()
+if stored_exception:
+    raise stored_exception[0], stored_exception[1], stored_exception[2]
+sys.exit()
